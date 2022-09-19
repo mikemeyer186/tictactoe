9f6af822392e280a6884b1a551f3ced30e39f545
Mike Meyer <mike@Mike-MacBookPro.fritz.box>
2022-09-19 20:33:06 +0200

    winning lines added

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 let fields = [];
 let currentPlayer = 'circle';
+let gameOver = false;
+
+let horizontalLineTop = 'top: 177px; transform: scale(1, 1)';
+let horizontalLineMid = 'top: 312px; transform: scale(1, 1)';
+let horizontalLineBottom = 'top: 447px; transform: scale(1, 1)';
+let verticalLineLeft = 'top: 316px; left: -265px; transform: scale(1, 1) rotate(90deg)';
+let verticalLineMid = 'top: 316px; transform: scale(1, 1) rotate(90deg)';
+let verticalLineRight = 'top: 316px; left: 273px; transform: scale(1, 1) rotate(90deg)';
+let diagonalLineLeft = 'width: 422px; top: 316px; left: 10px; transform: scale(1, 1) rotate(45deg)';
+let diagonalLineRight = 'width: 422px; top: 316px; left: -7px; transform: scale(1, 1) rotate(135deg)';
 
 
 function setSign(position) {
-    if (currentPlayer == 'cross') {
-        currentPlayer = 'circle';
-    } else {
-        currentPlayer = 'cross';
-    }
+    if (!gameOver) {
 
-    fields[position] = currentPlayer;
-    showSign(currentPlayer, position);
-    checkWinner();
-    setActivePlayer();
+        if (currentPlayer == 'cross') {
+            currentPlayer = 'circle';
+        } else {
+            currentPlayer = 'cross';
+        }
+
+        fields[position] = currentPlayer;
+        showSign(currentPlayer, position);
+        checkWinner();
+        setActivePlayer();
+    }
 }
 
 
@@ -28,6 +41,7 @@ function noPointerEvents(position) {
     field.classList.add('no-event');
 }
 
+
 function setActivePlayer() {
     document.getElementById('player-1').classList.toggle('player-inactive');
     document.getElementById('player-2').classList.toggle('player-inactive');
@@ -36,41 +50,66 @@ function setActivePlayer() {
 
 function checkWinner() {
     let winner;
+    let winnerLine = document.getElementById('winner-line');
 
     // horizontal rows
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        winnerLine.style = horizontalLineTop;
     }
 
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        winnerLine.style = horizontalLineMid;
     }
 
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        winnerLine.style = horizontalLineBottom;
     }
 
     // vertical rows
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        winnerLine.style = verticalLineLeft;
     }
 
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        winnerLine.style = verticalLineMid;
     }
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        winnerLine.style = verticalLineRight;
     }
 
     // diagonal rows
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        winnerLine.style = diagonalLineLeft;
     }
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        winnerLine.style = diagonalLineRight;
     }
 
-    console.log('Gewonnen:', winner);
+    if (winner) {
+        gameOver = true;
+        showWinnerScreen(winner);
+    }
+}
+
+
+function showWinnerScreen(winner) {
+    let winnerImage = document.getElementById('winner-image');
+    document.getElementById('player-box').classList.add('d-none');
+    document.getElementById('winner-image').classList.remove('d-none');
+
+    if (winner == 'cross') {
+        winnerImage.src = './img/win_player1.png';
+    } else {
+        console.log('Spieler 2 hat gewonnen');
+    }
 }
