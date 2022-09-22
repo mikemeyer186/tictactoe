85e4374197abf44757d5fc842c72c263262acb25
Mike Meyer <mike@Mike-MacBookPro.fritz.box>
2022-09-22 20:28:36 +0200

    final release

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ let currentPlayer = 'circle';
 let gameOver = false;
 let gameMove = 0;
 
+
+/** Sounds and volume */
 let endAudio = new Audio('./sounds/end_game.mp3');
 endAudio.volume = 0.5;
 
 
+/** Sets the sign of active player in array */
 function setSign(position) {
     if (!gameOver) {
         if (currentPlayer == 'cross') {
@@ -23,6 +26,7 @@ function setSign(position) {
 }
 
 
+/** Shows cross or circle of selected field */
 function showSign(currentPlayer, position) {
     let sign = document.getElementById(currentPlayer + position);
     sign.classList.remove('d-none');
@@ -30,12 +34,14 @@ function showSign(currentPlayer, position) {
 }
 
 
+/** Disables pointer events for selected field */
 function noPointerEvents(position) {
     let field = document.getElementById('tt-field' + position);
     field.classList.add('no-event');
 }
 
 
+/** Disables pointer evnets for all field */
 function noPointerEventsAllFields() {
     for (let i = 0; i < 9; i++) {
         document.getElementById('tt-field' + i).classList.add('no-event');
@@ -43,16 +49,18 @@ function noPointerEventsAllFields() {
 }
 
 
+/** Sets inactive player to transparent */
 function setActivePlayer() {
     document.getElementById('player-1').classList.toggle('player-inactive');
     document.getElementById('player-2').classList.toggle('player-inactive');
 }
 
 
+/** Checks winning fields */
 function checkWinner() {
     let winner;
 
-    // horizontal rows
+    /** horizontal rows */
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
         changeBgColor(0, 1, 2);
@@ -68,7 +76,7 @@ function checkWinner() {
         changeBgColor(6, 7, 8);
     }
 
-    // vertical rows
+    /** vertical rows */
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
         changeBgColor(0, 3, 6);
@@ -84,7 +92,7 @@ function checkWinner() {
         changeBgColor(2, 5, 8);
     }
 
-    // diagonal rows
+    /** diagonal rows */
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
         changeBgColor(0, 4, 8);
@@ -99,6 +107,7 @@ function checkWinner() {
 }
 
 
+/** Sets background-color to green if it is a winning field */
 function changeBgColor(f1, f2, f3) {
     document.getElementById(`tt-field${f1}`).classList.add('green-bg');
     document.getElementById(`tt-field${f2}`).classList.add('green-bg');
@@ -106,6 +115,7 @@ function changeBgColor(f1, f2, f3) {
 }
 
 
+/** Sets the winning screen */
 function gameEndScreen(winner) {
     if (winner) {
         endAudio.play();
@@ -119,20 +129,22 @@ function gameEndScreen(winner) {
 }
 
 
+/** Checks if no player wins */
 function noPlayerWins() {
     let winnerImage = document.getElementById('winner-image');
     gameMove++;
 
     if (gameMove == 9) {
-        document.getElementById('player-box').classList.add('transparent');
         winnerImage.src = './img/win_noplayer.png';
-        winnerImage.classList.add('scale');
+        document.getElementById('player-box').classList.add('transparent');
         gameOver = true;
         showRestartBtn();
+        winnerImage.classList.add('scale');
     }
 }
 
 
+/** Shows the winning image */
 function showWinnerScreen(winner) {
     let winnerImage = document.getElementById('winner-image');
     document.getElementById('player-box').classList.add('transparent');
@@ -147,11 +159,13 @@ function showWinnerScreen(winner) {
 }
 
 
+/** Shows the restart button */
 function showRestartBtn() {
     document.getElementById('restart-btn').classList.remove('d-none');
 }
 
 
+/** Restarts the game */
 function restartGame() {
     document.getElementById('winner-image').classList.remove('scale');
     document.getElementById('restart-btn').classList.add('d-none');
@@ -166,6 +180,7 @@ function restartGame() {
 }
 
 
+/** Clears the gamefield */
 function clearFields() {
     for (let i = 0; i < 9; i++) {
         document.getElementById('cross' + i).classList.add('d-none');
